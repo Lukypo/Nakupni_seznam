006a613ee6f243dfe42a7444ab88b0515f764a09
Lukypo <lukypotv@gmail.com>
2024-09-23 16:24:10 +0200

    main function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def add_item(item_list: list) -> None:
     item_list.append(item)
     print(f"Položka {item} byla přidána.")
 
+
 def remove_item(item_list: list) -> None:
     item: str = input("Zadejte název položky k odebrání: ")
     if item in item_list:
@@ -12,7 +13,21 @@ def remove_item(item_list: list) -> None:
         print(f"Položka není v seznamu")
 
 
-items: list = []
-add_item(items)
-add_item(items)
-remove_item(items)
+def main() -> None:
+    items: list = []
+    while True:
+        print(f"Přidat položku - 1 | Odebrat položku - 2 | Konec - 3")
+        command: int = int(input("Zadejte číslo příkazu: "))
+        if command == 1:
+            add_item(items)
+        elif command == 2:
+            remove_item(items)
+        elif command == 3:
+            print("Program byl ukončen.")
+            break
+        else:
+            print("Neplatný příkaz.")
+
+
+if __name__ == "__main__":
+    main()
