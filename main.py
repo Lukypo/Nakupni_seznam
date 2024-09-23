e01debc32d6cb225a5fead5fe2ac457be58b5492
petrlik lukas <petrlik.lukas>
2024-09-23 10:37:26 +0200

    remove item

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-def add_item(item_list):
-    item = input("Zadejte název položky k přidání: ")
+def add_item(item_list: list) -> None:
+    item: str = input("Zadejte název položky k přidání: ")
     item_list.append(item)
     print(f"Položka {item} byla přidána.")
+
+def remove_item(item_list: list) -> None:
+    item: str = input("Zadejte název položky k odebrání: ")
+    if item in item_list:
+        item_list.remove(item)
+        print(f"Položka {item} byla odebrána.")
+    else:
+        print(f"Položka není v seznamu")
+
+
+items: list = []
+add_item(items)
+add_item(items)
+remove_item(items)
