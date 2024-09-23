8c906e936192faef2bf630541d9ff2f289fcea59
Lukypo <lukypotv@gmail.com>
2024-09-23 16:37:18 +0200

    view items, sort, item count

--- main.py
+++ main.py
@@ -10,23 +10,49 @@ def remove_item(item_list: list) -> None:
         item_list.remove(item)
         print(f"Položka {item} byla odebrána.")
     else:
-        print(f"Položka není v seznamu")
+        print(f"Položka {item} není v seznamu")
+
+
+def view_items(item_list: list) -> None:
+    print("Seznam položek:")
+    for item in item_list:
+        print(item)
+
+
+def sort_items_alphabetically(item_list: list) -> None:
+    item_list.sort()
+    print("Seznam byl seřazen abecedně.")
+    view_items(item_list)
+
+
+def get_item_count(item_list: list) -> None:
+    print(f"Počet položek v seznamu: {len(item_list)}")
 
 
 def main() -> None:
     items: list = []
     while True:
-        print(f"Přidat položku - 1 | Odebrat položku - 2 | Konec - 3")
-        command: int = int(input("Zadejte číslo příkazu: "))
+        print(f"Přidat položku - 1 | Odebrat položku - 2 | Zobrazit položky - 3 | Seřadit položky - 4 | Počet položek - 5 | Konec - 6")
+        user_input: str = input("Zadejte číslo příkazu: ")
+        if not user_input.isdigit():
+            print("Neplatný příkaz")
+            continue
+
+        command: int = int(user_input)
         if command == 1:
             add_item(items)
         elif command == 2:
             remove_item(items)
         elif command == 3:
-            print("Program byl ukončen.")
+            view_items(items)
+        elif command == 4:
+            sort_items_alphabetically(items)
+        elif command == 5:
+            get_item_count(items)
+        elif command == 6:
             break
         else:
-            print("Neplatný příkaz.")
+            print("Neplatný příkaz")
 
 
 if __name__ == "__main__":
